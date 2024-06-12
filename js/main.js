7335b13b676ec589f67f78c834c938ad2de19779
Tommy Alhamra <tommy.alhamra@ist.id>
2024-06-12 23:14:31 +0700

    fix: update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function makeBook(obj) {
+function makeBook(obj, index) {
   const textTitle = document.createElement("h3");
   textTitle.innerText = "Book " + obj.title;
 
@@ -11,15 +11,42 @@ function makeBook(obj) {
   const buttonAction = document.createElement("div");
   buttonAction.classList.add("action");
 
-  const buttonDone = document.createElement("button");
-  buttonDone.classList.add("green");
-  buttonDone.innerText = "Done";
+  const buttonOptions = document.createElement("button");
+  buttonOptions.classList.add("green");
 
   const buttonDelete = document.createElement("button");
   buttonDelete.classList.add("red");
   buttonDelete.innerText = "Delete";
 
-  buttonAction.append(buttonDone, buttonDelete);
+  buttonDelete.addEventListener("click", function () {
+    showDelete(obj, index);
+  });
+
+  const buttonEdit = document.createElement("button");
+  buttonEdit.classList.add("orange");
+  buttonEdit.innerText = "Edit";
+
+  buttonEdit.addEventListener("click", function () {
+    showEditBook(obj, index);
+  });
+
+  if (!obj.isComplete) {
+    buttonOptions.innerText = "Done";
+    buttonAction.append(buttonOptions);
+
+    buttonOptions.addEventListener("click", function () {
+      addBookToCompleted(obj.id);
+    });
+  } else {
+    buttonOptions.innerText = "Ongoing";
+    buttonAction.append(buttonOptions);
+
+    buttonOptions.addEventListener("click", function () {
+      addBookToOngoing(obj.id);
+    });
+  }
+
+  buttonAction.append(buttonDelete, buttonEdit);
 
   const wrapArticle = document.createElement("article");
   wrapArticle.classList.add("book_item");
@@ -51,6 +78,8 @@ submitAction.addEventListener("submit", function (event) {
 
   bookList.push(objAdd);
 
+  submitAction.reset();
+
   document.dispatchEvent(new Event(RENDER_EVENT));
   event.preventDefault();
 });
@@ -67,8 +96,8 @@ document.addEventListener(RENDER_EVENT, function () {
   incompleteBookshelfList.innerHTML = "";
   completeBookshelfList.innerHTML = "";
 
-  for (const item of bookList) {
-    const objBook = makeBook(item);
+  for (const [index, item] of bookList.entries()) {
+    const objBook = makeBook(item, index);
 
     if (item.isComplete) {
       completeBookshelfList.append(objBook);
@@ -77,3 +106,91 @@ document.addEventListener(RENDER_EVENT, function () {
     }
   }
 });
+
+function addBookToCompleted(id) {
+  const bookTarget = findBook(id);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isComplete = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function addBookToOngoing(id) {
+  const bookTarget = findBook(id);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isComplete = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findBook(id) {
+  for (const item of bookList) {
+    if (item.id === id) {
+      return item;
+    }
+  }
+
+  return null;
+}
+
+const modalEdit = document.getElementById("isModalEdit");
+const span = document.getElementById("close");
+
+function showEditBook(obj, index) {
+  modalEdit.style.display = "block";
+
+  const form = document.getElementById("inputUpdateBook");
+
+  form.title.value = obj.title;
+  form.author.value = obj.author;
+  form.year.value = obj.year;
+  form.isCheckbox.checked = obj.isComplete;
+
+  const submitActionUpdate = document.getElementById("bookSubmitUpdate");
+
+  submitActionUpdate.addEventListener(
+    "click",
+    function (event) {
+      bookList[index] = {
+        id: obj.id,
+        title: form.title.value,
+        author: form.author.value,
+        year: form.year.value,
+        isComplete: form.isCheckbox.checked,
+      };
+
+      document.dispatchEvent(new Event(RENDER_EVENT));
+      event.preventDefault();
+      modalEdit.style.display = "none";
+    },
+    { once: true }
+  );
+}
+
+const modalDelete = document.getElementById("isModalDelete");
+
+function showDelete(obj, index) {
+  modalDelete.style.display = "block";
+
+  const submitActionDelete = document.getElementById("bookSubmitDelete");
+
+  submitActionDelete.addEventListener(
+    "click",
+    function (event) {
+      bookList.splice(index, 1);
+
+      document.dispatchEvent(new Event(RENDER_EVENT));
+      event.preventDefault();
+
+      modalDelete.style.display = "none";
+    },
+    { once: true }
+  );
+}
+
+span.onclick = function () {
+  modalEdit.style.display = "none";
+  modalDelete.style.display = "none";
+};
